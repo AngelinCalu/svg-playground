1ffad06adffeba9bc7b4b70ae44d27038307e53b
Angelin Calu <angelin.calu@gmail.com>
2020-12-28 05:02:35 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function App() {
+const App: React.FC = (): JSX.Element => {
   return (
     <div className="w-full h-screen flex items-center">
       <div className="mx-auto">Start</div>
     </div>
   );
-}
+};
 
 export default App;
