6624dddee1d4ed8d51726e6226156b7c8f8a2dc6
Angelin Calu <angelin.calu@gmail.com>
2020-12-30 02:54:39 +0200

    playing with paths

--- src/app/animation/index.tsx
+++ src/app/animation/index.tsx
@@ -1,11 +1,41 @@
 import React, { useRef, useEffect } from "react";
 
+interface Point2D {
+  x: number;
+  y: number;
+}
+
 const Animation: React.FC = (): JSX.Element => {
   const radius = 240;
   const diameter = radius * 2;
-  const strokeWidth = 2;
+  const strokeWidth = 3;
+  const thinStrokeWidth = 1;
   const strokeColor = "#172A4D";
   const centerColor = "#cc0000";
+
+  // Origin for Right Circle
+  const OR: Point2D = {
+    x: diameter * 2,
+    y: radius + radius / 2,
+  };
+
+  // Origin for Left Circle
+  const OL: Point2D = {
+    x: radius,
+    y: radius + radius / 2,
+  };
+
+  const P1L: Point2D = {
+    x: strokeWidth,
+    y: radius + radius / 2,
+  };
+
+  const P1R: Point2D = {
+    x: diameter + radius + strokeWidth,
+    y: radius + radius / 2,
+  };
+
+  const notationOffset = 16;
 
   const circle1Path = useRef<SVGPathElement>(null);
   const circle2Path = useRef<SVGPathElement>(null);
@@ -36,7 +66,7 @@ const Animation: React.FC = (): JSX.Element => {
   //   }, []);
 
   return (
-    <div className="fixed mx-auto top-10 left-20 md:left-1/3">
+    <div className="fixed mx-auto top-10 left-20 md:left-1/4">
       <svg
         // ref={SVGElement}
         fill="none"
@@ -45,13 +75,13 @@ const Animation: React.FC = (): JSX.Element => {
             ${diameter * 2 + radius + 2 * strokeWidth} 
             ${diameter * 2 + radius + 2 * strokeWidth}
             `}
-        width={diameter}
-        height={diameter}
+        width={radius * 3}
+        height={radius * 3}
       >
         <text
-          fontFamily="Verdana, tahoma"
           fill={strokeColor}
-          fontSize="4rem"
+          fontFamily="'Carter One', cursive"
+          fontSize="4.5rem"
           x="50%"
           y="40"
           dominantBaseline="middle"
@@ -59,30 +89,111 @@ const Animation: React.FC = (): JSX.Element => {
         >
           Angelin Calu
         </text>
+
+        <text
+          fill={strokeColor}
+          x={OR.x + notationOffset}
+          y={OR.y + notationOffset}
+          dominantBaseline="middle"
+          textAnchor="middle"
+        >
+          OR
+        </text>
+
+        <text
+          fill={strokeColor}
+          x={OL.x + notationOffset}
+          y={OL.y + notationOffset}
+          dominantBaseline="middle"
+          textAnchor="middle"
+        >
+          OL
+        </text>
+
+        {/* Center of the right circle */}
         <circle
           fill={centerColor}
           strokeWidth={0}
           r={strokeWidth}
-          cx={diameter + diameter}
-          cy={radius}
+          cx={OR.x}
+          cy={OR.y}
         />
+
+        <text
+          fill={strokeColor}
+          x={P1L.x + notationOffset}
+          y={P1L.y + notationOffset}
+          textAnchor="middle"
+        >
+          P1L
+        </text>
+
+        <text
+          fill={strokeColor}
+          x={P1R.x + notationOffset}
+          y={P1R.y + notationOffset}
+          textAnchor="middle"
+        >
+          P1R
+        </text>
+
+        {/* Center of the left circle */}
         <circle
           fill={centerColor}
           strokeWidth={0}
           r={strokeWidth}
-          cx={radius}
-          cy={radius}
+          cx={OL.x}
+          cy={OL.y}
         />
+
+        {/* Starting point for drawing left circle */}
+        <circle
+          fill={centerColor}
+          strokeWidth={0}
+          r={strokeWidth}
+          cx={P1L.x}
+          cy={P1L.y}
+        />
+
+        {/* Starting point for drawing second circle */}
+        <circle
+          fill={centerColor}
+          strokeWidth={0}
+          r={strokeWidth}
+          cx={P1R.x}
+          cy={P1R.y}
+        />
+
         <path
           ref={circle1Path}
           fill="none"
           stroke={strokeColor}
           strokeWidth={strokeWidth}
           d={`
-            M ${radius + strokeWidth} ${radius + strokeWidth}
+            M ${radius + strokeWidth} ${radius + radius / 2 + strokeWidth}
             m ${-1 * radius}, 0
             a ${radius},${radius} 0 1,0 ${radius * 2},0
             a ${radius},${radius} 0 1,0 ${radius * -2},0
+            `}
+        />
+
+        <path
+          fill="none"
+          stroke={strokeColor}
+          strokeWidth={thinStrokeWidth}
+          d={`
+            M ${radius + strokeWidth} ${radius + radius / 2}
+            L ${strokeWidth} ${radius + radius / 2}
+            `}
+        />
+
+        <path
+          fill="none"
+          stroke={strokeColor}
+          strokeWidth={thinStrokeWidth}
+          d={`
+            M ${diameter * 2 + strokeWidth} ${radius + radius / 2}
+            L ${radius * 3 + strokeWidth} ${radius + radius / 2}
             `}
         />
 
@@ -92,7 +203,7 @@ const Animation: React.FC = (): JSX.Element => {
           stroke={strokeColor}
           strokeWidth={strokeWidth}
           d={`
-            M ${2 * diameter + strokeWidth} ${radius + strokeWidth}
+            M ${2 * diameter + strokeWidth} ${radius + radius / 2 + strokeWidth}
             m ${-1 * radius}, 0
             a ${radius},${radius} 0 1,0 ${radius * 2},0
             a ${radius},${radius} 0 1,0 ${radius * -2},0
